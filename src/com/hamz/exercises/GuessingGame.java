381aba19b21259dc8b236f244584230cd264dcfd
Muhammed Hamza Toptancı <toptanci.hamza@gmail.com>
2024-07-18 19:50:18 +0300

    Sixth Day - Exercise 1

---
+++ src/com/hamz/exercises/GuessingGame.java
@@ -0,0 +1,32 @@
+package com.hamz.exercises;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100) + 1;
+        int numberOfTries = 0;
+        int guess;
+        boolean win = false;
+
+        System.out.println("Guess a number between 1 and 100:");
+
+        while (!win) {
+            guess = scanner.nextInt();
+            numberOfTries++;
+
+            if (guess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                win = true;
+                System.out.println("Correct! You win!");
+                System.out.println("It took you " + numberOfTries + " tries.");
+            }
+        }
+        scanner.close();
+    }
+}
